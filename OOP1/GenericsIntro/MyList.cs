5b5d4be1f095154daff54f40becc4185f9f76942
Leyla <leylay1996@gmail.com>
2021-01-26 23:46:32 +0300

    added generics

---
+++ OOP1/GenericsIntro/MyList.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GenericsIntro
+{
+    //generic class çalışcagımız öel bir type'ın 
+    //olacagını belirtiyoruz
+    class MyList<T>
+    { 
+        T[] items;
+
+        //constructor->ilk başlangıcında çalışır
+        public MyList()
+        {
+            items = new T[0];
+        }
+        public void Add(T item)
+        {
+            T[] tempArray = items;
+            items = new T[items.Length + 1];
+        }
+    }
+}
