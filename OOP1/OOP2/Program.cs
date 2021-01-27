829c2451c078b923da4ad3de2124d1e9bb0387f8
Leyla <leylay1996@gmail.com>
2021-01-27 23:47:38 +0300

    customer added

---
+++ OOP1/OOP2/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace OOP2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Customer musteri1 = new Customer();
+            musteri1.Adi = "Leyla";
+            musteri1.Id = 1;
+            musteri1.MusteriNo = "2222222";
+            musteri1.TcNo = "1234343";
+            musteri1.SirketAdi = "?";
+
+            //gercek-tüzel müşteri
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
