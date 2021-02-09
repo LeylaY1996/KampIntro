3f708c071ffea78e68031112b3b0ce8b23e982dc
Leyla <leylay1996@gmail.com>
2021-02-09 19:26:41 +0300

    created sql

---
+++ OOP1/SQLQuery2.sql
@@ -0,0 +1,8 @@
+﻿--select
+--Select ContactName from Customers;
+
+Select * from Customers where City = 'Berlin';
+
+Select * from Products where CategoryID=1 or CategoryID=3
+
+Select * from Products where CategoryID=1 and UnitPrice>10
